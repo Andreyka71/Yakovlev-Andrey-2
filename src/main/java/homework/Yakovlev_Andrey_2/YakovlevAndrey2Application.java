93261443f426a1b5b178a0e075a98cea3c98d52e
Andreyka71 <andrey.yakovlev.al@gmail.com>
2025-04-09 12:26:33 +0300

    init

---
+++ src/main/java/homework/Yakovlev_Andrey_2/YakovlevAndrey2Application.java
@@ -0,0 +1,13 @@
+package homework.Yakovlev_Andrey_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakovlevAndrey2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YakovlevAndrey2Application.class, args);
+	}
+
+}
